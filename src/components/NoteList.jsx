a39afe0b69c14111255ae7c69d2a102a7cb40de2
liguangshu <13590007313@163.com>
2025-07-23 17:59:47 +0800

    修改文案

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -3,7 +3,7 @@ import NoteCard from "./NoteCard";
 
 const NoteList = ({ notes, onDeleteNote }) => {
   if (!notes.length) {
-    return <div className="text-gray-400 text-center py-8">暂无便签</div>;
+    return <div className="text-gray-400 text-center py-8">暂无便签11111</div>;
   }
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
